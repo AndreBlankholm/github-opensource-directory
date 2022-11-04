93eeff0215315c9f4c5e92245a314f8fa8234636
AndreBlankholm <ablankholm@gmail.com>
2022-11-03 23:51:07 -0500

    form submit creation

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,42 @@
+var nameInputEl = document.querySelector("#username");
+var userFormEl = document.querySelector("#user-form");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
-var getUserRepos = function(user) {
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data){
-            console.log(data);
-        });
-    });
-    
+
+var getUserRepos = function (user) {
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function(data) {
+        displayRepos(data, user);
+      });
+  });
 };
-  
-getUserRepos("AndreBlankholm");
+
+
+
+
+
+
+//form on submit
+var formSubmitHandeler = function(event) {
+  event.preventDefault();
+
+  var username = nameInputEl.value.trim();
+
+  if(username){
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username!");
+  }
+  console.log(event);
+};
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+  };
+
+
+userFormEl.addEventListener("submit", formSubmitHandeler);
