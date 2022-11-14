e3cad6535cdd16423f33931517146af73e84f65f
AndreBlankholm <ablankholm@gmail.com>
2022-11-14 14:34:59 -0600

    added readme and screenshot

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # github-opensource-directory
-A  web app that will search GitHub for open source projects with open issues and pull requests.
+## A web app that will search GitHub for open source projects, dispaly them and the open issues.
+
+
+Screenshot:
+# <img src="./assets/images/githubOpenSRC.png"   alt="picture of open source directory app " >
+
+
+## Link: https://andreblankholm.github.io/github-opensource-directory/
+
+The MIT license 
+## ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+### License & copyright 2022 © Andre Blankholm
--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,22 +5,33 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var getUserRepos = function (user) {
+
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      displayRepos(data, user);
-    });
+
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data, user);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  })
+  .catch(function(error) {  //catch for network errors
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
   });
 };
 
-//form on submit
+//form on submit 
 var formSubmitHandeler = function (event) {
   event.preventDefault();
 
   var username = nameInputEl.value.trim();
 
   if (username) {
-    getUserRepos(username);
+    getUserRepos(username);   //passe scurrent username to the getUserRepose function call
     nameInputEl.value = "";
   } else {
     alert("Please enter a GitHub username!");
@@ -29,6 +40,14 @@ var formSubmitHandeler = function (event) {
 };
 
 var displayRepos = function (repos, searchTerm) {
+
+  //error handling
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
+
   // clears out old content (first thing)
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
@@ -51,7 +70,22 @@ var displayRepos = function (repos, searchTerm) {
     //append child to container
     repoEl.appendChild(titleEl);
 
-    // append container to the dom
+    //create a stutus element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row alighn-center";
+
+    //check to see if there is any issues still open
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    //append to the container
+    repoEl.appendChild(statusEl);
+
+    // append container and every thing in it to the dom
     repoContainerEl.appendChild(repoEl);
   }
 };
